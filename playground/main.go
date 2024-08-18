9413f5ca8277514ff1562fcd94fced46094c350a
HeuristicHiker <connor.dev.johnston@gmail.com>
2024-08-18 15:56:23 -0700

    Addition of structs basics

--- playground/main.go
+++ playground/main.go
@@ -4,15 +4,16 @@ import "fmt"
 
 func main() {
 	// testList := string[]{"One", "Two", "Three"}
-	testString := "One"
+	// testString := "One"
+	fmt.Printf("%U", "Hello")
 
 	// fmt.Println(testString)
 	// addressOfTest := &testString
-	newAdr := replaceString(testString)
+	// newAdr := replaceString(testString)
 	// testString = "Two"
 	// fmt.Println(&testString)
-	fmt.Println("out fun ", *newAdr, &newAdr)
-	fmt.Println("last fun ", testString, &testString)
+	// fmt.Println("out fun ", *newAdr, &newAdr)
+	// fmt.Println("last fun ", testString, &testString)
 }
 
 func replaceString(strPtr string) *string {
