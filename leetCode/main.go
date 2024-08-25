b8af9a258c18737698ec46c181f6170c6a6d1bfe
HeuristicHiker <connor.dev.johnston@gmail.com>
2024-08-25 15:41:19 -0400

    additional challenge solved

--- leetCode/main.go
+++ leetCode/main.go
@@ -9,13 +9,11 @@ import (
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
 Increment the large integer by one and return the resulting array of digits.
-
-
 */
 
 func main() {
 	// plusOne([]int{9})
-	fmt.Println(canMakeArithmeticProgression([]int{3, 5, 1}))
+	fmt.Println(isMonotonic([]int{1, 2, 2, 4}))
 }
 
 func plusOne(digits []int) []int {
@@ -37,12 +35,35 @@ func plusOne(digits []int) []int {
 func canMakeArithmeticProgression(arr []int) bool {
 	sort.Ints(arr)
 
-	dif := arr[1] - arr[0]
+	stepFunction := arr[1] - arr[0]
 
 	for i := 1; i < len(arr); i++ {
-		if dif != arr[i]-arr[i-1] {
+		if stepFunction != arr[i]-arr[i-1] {
 			return false
 		}
 	}
+	return true
+}
+
+func isMonotonic(nums []int) bool {
+	isIncreasing, isDecreasing := false, false
+	length := len(nums)
+
+	if length < 2 {
+		return false
+	}
+
+	for i := 1; i < len(nums); i++ {
+
+		if nums[i-1] > nums[i] {
+			isIncreasing = true
+		} else if nums[i-1] < nums[i] {
+			isDecreasing = true
+		}
+		if isIncreasing && isDecreasing {
+			return false
+		}
+	}
+
 	return true
 }
